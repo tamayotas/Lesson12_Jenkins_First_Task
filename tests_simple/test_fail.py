9d9ab1a99b5bb1cff417c5a18ff21c11e988a7ae
tamayotas <tamaevs@gmail.com>
2024-02-15 22:20:54 +0300

    added simple tests

---
+++ tests_simple/test_fail.py
@@ -0,0 +1,38 @@
+def test_fail():
+    assert False
+
+
+def test_pass1():
+    assert False
+
+
+def test_pass2():
+    assert False
+
+
+def test_pass3():
+    assert False
+
+
+def test_pass4():
+    assert False
+
+
+def test_pass5():
+    assert False
+
+
+def test_pass6():
+    assert False
+
+
+def test_pass7():
+    assert False
+
+
+def test_pass8():
+    assert False
+
+
+def test_pass9():
+    assert False
